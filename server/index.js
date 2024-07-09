b88ab1bacf49fbeb822390f3a953db2fa51b44d2
Prem <premkhodke.99@gmail.com>
2024-07-09 14:36:32 +0530

    start app

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const router = require("./routes/git.routes");
+const dbConnect = require("./db/dbConfig");
+
+dbConnect();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/', router)
+
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log('connected at', PORT);
+})
