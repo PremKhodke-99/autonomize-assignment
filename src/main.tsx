9b65df23cf70d50f29e12ad12b4098427516045a
Prem <premkhodke.99@gmail.com>
2024-07-10 01:29:17 +0530

    redux removed

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-
-import { Provider } from 'react-redux'
-import  store  from './appStore/store.ts'
 import { BrowserRouter } from 'react-router-dom'
 import Navbar from './components/Navbar.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <BrowserRouter>
-        <Navbar />
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>,
-  </Provider>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Navbar />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
